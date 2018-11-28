f3a913760a4e07c0e838803c705d53d75e66258f
VictorAliaga <vict.aliaga@gmail.com>
2018-11-28 00:29:21 -0300

    Agrega Cliente

---
+++ app/parse.js
@@ -0,0 +1,19 @@
+var xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        var response = JSON.parse(xhttp.responseText);
+        var perro = response;
+
+        var output = '';
+        for (var i = 0; i < perro.length; i++) {
+            output += '<article>' + '<h2><b>'+perro[i].NombreMascota+'</h2></b>' + '<img src="'+perro[i].Imagen+'" alt="'+i+'">' +
+            '<p><b>Raza: </b>' +perro[i].RazaPredominante+'</p>' + '<p><b>Description: </b>' +perro[i].Descripcion+'</p>' + '<p><b>Estado: </b>' +perro[i].Estado+'</p></article>'; 
+        }
+
+        document.getElementById('MascotasContenedor').innerHTML = output;
+    }
+};
+
+xhttp.open("GET", "http://victoraliaga.pythonanywhere.com/mascotas/?format=json", true);
+xhttp.send();
---
+++ service-worker.js
@@ -0,0 +1,66 @@
+var cacheName = "misperris";
+var filesToCache = [
+    "/",
+    "/index.html",
+    "/mascotas.html",
+    "/registro.html",
+    "/login.html",
+    "/app/app.js",
+    "/app/parse.js",
+    "/font/fontello.eot",
+    "/font/fontello.svg",
+    "/font/fontello.ttf",
+    "/font/fontello.woff",
+    "/font/fontello.woff2",
+    "/style/banner.css",
+    "/style/fontello.css",
+    "/style/login.css",
+    "/style/main.css",
+    "/style/menu.css", 
+    "/style/perritodetalle.css",
+    "/style/perritos.css",
+    "/style/registrar.css",
+    "/style/registrocompleto.css",
+    "/img/banner.css",
+    "/img/favicon.css",
+    "/img/icono.png",
+    "/img/icono-125x125.png",
+    "/img/icono-152x152.png",
+    "/img/icono-192x192.png",
+
+  
+];
+
+self.addEventListener( 'install', function( e ) {
+    console.log( '[ServiceWorker] Install' );
+    e.waitUntil(
+        caches.open( cacheName ).then( function( cache ) {
+            console.log( '[ServiceWorker] Caching app shell' );
+            return cache.addAll( filesToCache );
+        } )
+    );
+} );
+
+self.addEventListener( 'activate', function( e ) {
+    console.log( '[ServiceWorker] Activate' );
+    e.waitUntil(
+        caches.keys( ).then( function( keyList ) {
+            return Promise.all( keyList.map( function( key ) {
+                if ( key != cacheName ) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete( key );
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener( 'fetch', function( e ) {
+    console.log( '[ServiceWorker] Fetch', e.request.url );
+    e.respondWith(
+        caches.match( e.request ).then( function( response ) {
+            return response || fetch( e.request );
+        } )
+    );
+} );
